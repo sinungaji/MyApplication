f8fe7c31f82cc4fa0a46cb2a1271f88155f0ca69
Sinung <sinung.purnama@ti.ukdw.ac.id>
2020-11-05 03:29:48 +0700

    Update Bagian Main Activity dan pengumuman pribadi, umum

--- app/src/main/java/com/example/myapplication/PengumumanActivity.java
+++ app/src/main/java/com/example/myapplication/PengumumanActivity.java
@@ -1,14 +1,176 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.bumptech.glide.Glide;
+import com.example.myapplication.helper.Config;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class PengumumanActivity extends AppCompatActivity {
+    private ImageView profileUser, imgBack, tumbnailpengumumanumum;
+    private ProgressDialog pDialog;
+    String id_pengumuman;
+
+    private TextView judulpengumumanInput, isipengumumanInput, tgltayangpengumumanInput, tglberakhirpengumumanInput;
+    private String judulpengumumanString, isipengumumanString, tgltayangpengumumanString, tglberakhirpengumumanString, tumbnailpengumumanumumString;
+
+    private String judulpengumumanStringBerkas, isipengumumanStringBerkas, tgltayangpengumumanStringBerkas, tglberakhirpengumumanStringBerkas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengumuman);
+        Bundle bundle = getIntent().getExtras();
+        id_pengumuman = bundle.getString("id_pengumuman");
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        imgBack = findViewById(R.id.imgBack);
+        tumbnailpengumumanumum = findViewById(R.id.tumbnailpengumumanumum);
+        judulpengumumanInput = findViewById(R.id.inputJudulPengumuman);
+        isipengumumanInput = findViewById(R.id.inputisipengumuman);
+        tgltayangpengumumanInput = findViewById(R.id.inputtgltayangpengumuman);
+        tglberakhirpengumumanInput = findViewById(R.id.inputtglberakhirpengumuman);
+
+        actionButton();
+        getDataPengumuman();
+        Log.d("data1", id_pengumuman);
+    }
+
+    private void actionButton() {
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getDataPengumuman() {
+        // Tag used to cancel the request
+        pDialog.setMessage("Pengumuman Pribadi ...");
+        showDialog();
+        AndroidNetworking.post(Config.url + "pengumuman/detail")
+                .addBodyParameter("id_pengumuman", id_pengumuman)
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        hideDialog();
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject responses = response.getJSONObject(i);
+                                tumbnailpengumumanumumString = responses.getString("Thumbnail");
+                                judulpengumumanString = responses.getString("judul_pengumuman");
+                                isipengumumanString = responses.getString("isi_pengumuman");
+                                tgltayangpengumumanString = responses.getString("tgl_tayang_pengumuman");
+                                tglberakhirpengumumanString = responses.getString("tgl_berakhir_pengumuman");
+
+                            }
+
+                            deklarasidata();
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        hideDialog();
+                        Log.d("data1", "onError errorCode : " + error.getErrorCode());
+                        Log.d("data1", "onError errorBody : " + error.getErrorBody());
+                        Log.d("data1", "onError errorDetail : " + error.getErrorDetail());
+
+                        if (error.getErrorCode() == 400) {
+                            try {
+                                JSONObject body = new JSONObject(error.getErrorBody());
+                                AlertDialog.Builder builder1 = new AlertDialog.Builder(PengumumanActivity.this);
+                                builder1.setMessage(body.optString("pesan"));
+                                builder1.setCancelable(false);
+
+                                builder1.setPositiveButton(
+                                        "Oke",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                dialog.cancel();
+
+                                            }
+                                        });
+
+                                AlertDialog alert11 = builder1.create();
+                                alert11.show();
+                            } catch (JSONException ignored) {
+
+                            }
+
+                        } else {
+                            // error.getErrorDetail() : connectionError, parseError, requestCancelledError
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(PengumumanActivity.this);
+                            builder1.setMessage("Jaringan sedang sibuk. Klik 'Ok', untuk menutup pesan ini!");
+                            builder1.setCancelable(false);
+
+                            builder1.setPositiveButton(
+                                    "Oke",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+
+                                        }
+                                    });
+
+                            AlertDialog alert11 = builder1.create();
+                            alert11.show();
+                        }
+
+                    }
+                });
+
+    }
+
+    private void deklarasidata() {
+        Glide.with(PengumumanActivity.this).load(tumbnailpengumumanumumString).into(tumbnailpengumumanumum);
+        judulpengumumanInput.setText(judulpengumumanString);
+        isipengumumanInput.setText(isipengumumanString);
+        tgltayangpengumumanInput.setText(tgltayangpengumumanString);
+        tglberakhirpengumumanInput.setText(tglberakhirpengumumanString);
+    }
+
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
--- app/src/main/java/com/example/myapplication/Search.java
+++ app/src/main/java/com/example/myapplication/Search.java
@@ -15,9 +15,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +29,7 @@ import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.example.myapplication.helper.Config;
 import com.bumptech.glide.Glide;
 import com.example.myapplication.helper.Config;
 import com.example.myapplication.model.MenuPengumumanPribadi;
@@ -39,197 +44,65 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    ArrayList<MenuPengumumanPribadi> MenuPengumumanPribadi;
-    RecyclerView recyclerviewpengumumanpribadi;
-
+public class Search extends AppCompatActivity {
     ArrayList<MenuPengumumanUmum> MenuPengumumanUmum;
+
     RecyclerView recycleviepengumumanumum;
 
+    private ImageView imgBack;
     private ProgressDialog pDialog;
-    private ImageView profileUser, searchView;
-
+    private EditText et_search;
+    String visibilitas, limit, offset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
 
         pDialog = new ProgressDialog(this);
         pDialog.setCancelable(false);
 
-        searchView = findViewById(R.id.imgSearch);
-
-        recyclerviewpengumumanpribadi = findViewById(R.id.recycleviepengumumanpribadi);
+        imgBack = findViewById(R.id.imgBack);
+        et_search = findViewById(R.id.et_search);
         recycleviepengumumanumum = findViewById(R.id.recycleviepengumumanumum);
 
-        MenuPengumumanPribadi = new ArrayList<>();
-        LinearLayoutManager b = new GridLayoutManager(getApplicationContext(), 1, GridLayoutManager.HORIZONTAL, false);
-        recyclerviewpengumumanpribadi.setHasFixedSize(true);
-        recyclerviewpengumumanpribadi.setLayoutManager(b);
-        recyclerviewpengumumanpribadi.setNestedScrollingEnabled(true);
+        et_search.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(et_search, InputMethodManager.SHOW_IMPLICIT);
 
         MenuPengumumanUmum = new ArrayList<>();
-        LinearLayoutManager c = new GridLayoutManager(getApplicationContext(), 1, GridLayoutManager.VERTICAL, false);
+        LinearLayoutManager x = new GridLayoutManager(getApplicationContext(), 1, GridLayoutManager.VERTICAL, false);
         recycleviepengumumanumum.setHasFixedSize(true);
-        recycleviepengumumanumum.setLayoutManager(c);
+        recycleviepengumumanumum.setLayoutManager(x);
         recycleviepengumumanumum.setNestedScrollingEnabled(true);
 
         actionButton();
-        getDataPengumumanPribadi();
-        getDataPengumumanUmum();
     }
 
-
-
     private void actionButton() {
-        searchView.setOnClickListener(new View.OnClickListener() {
+        imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Search.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
-    }
-
-    private void getDataPengumumanPribadi() {
-        pDialog.setMessage("Pengumuman Pribadi ...");
-        MenuPengumumanPribadi.clear();
-        showDialog();
-        AndroidNetworking.post(Config.url+"pengumuman/list")
-                .addBodyParameter("visibilitas", "1")
-                .addBodyParameter("limit", "0")
-                .addBodyParameter("offset", "5")
-                .addBodyParameter("pencarian", "")
-                .setPriority(Priority.LOW)
-                .build()
-                .getAsJSONArray(new JSONArrayRequestListener() {
-                    @Override
-                    public void onResponse(JSONArray responseB) {
-                        hideDialog();
-                        try {
-                            for (int i = 0; i < responseB.length(); i++) {
-                                JSONObject responsesB = responseB.getJSONObject(i);
-                                Log.d("hehe", responsesB.optString("id_pengumuman"));
-                                MenuPengumumanPribadi pp = new MenuPengumumanPribadi(
-                                        responsesB.getString("id_pengumuman"),
-                                        responsesB.getString("judul_pengumuman"),
-                                        responsesB.getString("isi_pengumuman"),
-                                        responsesB.getString("tgl_tayang_pengumuman"),
-                                        responsesB.getString("tgl_berakhir_pengumuman"),
-                                        responsesB.getString("Thumbnail"));
-
-                                MenuPengumumanPribadi.add(pp);
-                            }
-                            MenuAdapterPengumumanPribadi menuAdapterPengumumanPribadi = new MenuAdapterPengumumanPribadi(getApplicationContext(), MenuPengumumanPribadi);
-                            recyclerviewpengumumanpribadi.setAdapter(menuAdapterPengumumanPribadi);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-
-                    @Override
-                    public void onError(ANError error) {
-                        // handle error
-                        hideDialog();
-                        Log.d("data1", "onError errorCode : " + error.getErrorCode());
-                        Log.d("data1", "onError errorBody : " + error.getErrorBody());
-                        Log.d("data1", "onError errorDetail : " + error.getErrorDetail());
-
-                        if (error.getErrorCode() == 400) {
-                            try {
-                                JSONObject body = new JSONObject(error.getErrorBody());
-                                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
-                                builder1.setMessage(body.optString("pesan"));
-                                builder1.setCancelable(false);
-
-                                builder1.setPositiveButton(
-                                        "Oke",
-                                        new DialogInterface.OnClickListener() {
-                                            public void onClick(DialogInterface dialog, int id) {
-                                                dialog.cancel();
-
-                                            }
-                                        });
-
-                                AlertDialog alert11 = builder1.create();
-                                alert11.show();
-                            } catch (JSONException ignored) {
-
-                            }
-
-                        } else {
-                            // error.getErrorDetail() : connectionError, parseError, requestCancelledError
-                            AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
-                            builder1.setMessage("Jaringan sedang sibuk. Klik 'Ok', untuk menutup pesan ini!");
-                            builder1.setCancelable(false);
-
-                            builder1.setPositiveButton(
-                                    "Oke",
-                                    new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int id) {
-                                            dialog.cancel();
-
-                                        }
-                                    });
-
-                            AlertDialog alert11 = builder1.create();
-                            alert11.show();
-                        }
-
-                    }
-                });
-    }
-    public class MenuAdapterPengumumanPribadi extends RecyclerView.Adapter<MenuAdapterPengumumanPribadi.ProductViewHolder> {
-        private Context mCtb;
-        private List<MenuPengumumanPribadi> MenuPengumumanPribadi;
-
-        MenuAdapterPengumumanPribadi(Context mCtb, List<MenuPengumumanPribadi> MenuPegumumanPribadi) {
-            this.mCtb = mCtb;
-            this.MenuPengumumanPribadi = MenuPegumumanPribadi;
-        }
-
-        @NonNull
-        @Override
-        public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroupB, int b) {
-            LayoutInflater inflaterB = LayoutInflater.from(mCtb);
-            @SuppressLint("InflateParams") View viewB = inflaterB.inflate(R.layout.activity_pengumuman_pribadi, null);
-            return new ProductViewHolder(viewB);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ProductViewHolder holderB, int b) {
-            final com.example.myapplication.model.MenuPengumumanPribadi menuPengumumanPribadi = MenuPengumumanPribadi.get(b);
-            Glide.with(MainActivity.this).load(menuPengumumanPribadi.getThumbnail()).into(holderB.tumbnailpengumumanpribadi);
-            holderB.text_judul_pengumuman_pribadi.setText(menuPengumumanPribadi.getJudul_pengumuman());
-            holderB.text_isi_pengumuman_pribadi.setText(menuPengumumanPribadi.getIsi_pengumuman());
-            holderB.text_tgl_tayang_pengumuman_pribadi.setText(menuPengumumanPribadi.getTgl_tayang_pengumuman());
-        }
-
-        @Override
-        public int getItemCount() {
-            return MenuPengumumanPribadi.size();
-        }
-
-        class ProductViewHolder extends RecyclerView.ViewHolder {
-            TextView text_judul_pengumuman_pribadi, text_isi_pengumuman_pribadi, text_tgl_tayang_pengumuman_pribadi;
-            CardView cvPengumumanPribadi;
-            ImageView tumbnailpengumumanpribadi;
-
-            ProductViewHolder(View itemViewB) {
-                super(itemViewB);
-                cvPengumumanPribadi = itemViewB.findViewById(R.id.cvPengumumanPribadi);
-                tumbnailpengumumanpribadi = itemView.findViewById(R.id.tumbnailpengumumanpribadi);
-                text_judul_pengumuman_pribadi = itemViewB.findViewById(R.id.judulpengumumanPribadiTV);
-                text_isi_pengumuman_pribadi = itemViewB.findViewById(R.id.isipengumumanPribadiTV);
-                text_tgl_tayang_pengumuman_pribadi = itemViewB.findViewById(R.id.tgltayangpengumumanPribadiTV);
-
+        et_search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH){
+                    MenuPengumumanUmum.clear();
+                    getDataPengumumanUmum();
+                    return true;
+                }
+                return false;
             }
-        }
+        });
+
     }
 
     private void getDataPengumumanUmum() {
+        // Tag used to cancel the request
         pDialog.setMessage("Pengumuman Bawah ...");
         MenuPengumumanUmum.clear();
         showDialog();
@@ -237,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 .addBodyParameter("visibilitas", "0")
                 .addBodyParameter("limit", "0")
                 .addBodyParameter("offset", "5")
-                .addBodyParameter("pencarian", "")
+                .addBodyParameter("pencarian", et_search.getText().toString().trim())
                 .setPriority(Priority.LOW)
                 .build()
                 .getAsJSONArray(new JSONArrayRequestListener() {
@@ -247,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             for (int i = 0; i < response.length(); i++) {
                                 JSONObject responses = response.getJSONObject(i);
-                                Log.d("hehe", responses.optString("id_pengumuman"));
                                 MenuPengumumanUmum bk = new MenuPengumumanUmum(
                                         responses.getString("id_pengumuman"),
                                         responses.getString("judul_pengumuman"),
@@ -279,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                         if (error.getErrorCode() == 400) {
                             try {
                                 JSONObject body = new JSONObject(error.getErrorBody());
-                                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                                AlertDialog.Builder builder1 = new AlertDialog.Builder(Search.this);
                                 builder1.setMessage(body.optString("pesan"));
                                 builder1.setCancelable(false);
 
@@ -300,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
 
                         } else {
                             // error.getErrorDetail() : connectionError, parseError, requestCancelledError
-                            AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(Search.this);
                             builder1.setMessage("Jaringan sedang sibuk. Klik 'Ok', untuk menutup pesan ini!");
                             builder1.setCancelable(false);
 
@@ -333,17 +205,17 @@ public class MainActivity extends AppCompatActivity {
 
         @NonNull
         @Override
-        public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        public MenuAdapter.ProductViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
             LayoutInflater inflater = LayoutInflater.from(mCtc);
             @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.activity_pengumuman_umum, null);
-            return new ProductViewHolder(view);
+            return new MenuAdapter.ProductViewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull ProductViewHolder holder, int i) {
+        public void onBindViewHolder(@NonNull MenuAdapter.ProductViewHolder holder, int i) {
             final com.example.myapplication.model.MenuPengumumanUmum menu = MenuPengumumanUmum.get(i);
+            Glide.with(Search.this).load(menu.getThumbnail()).into(holder.tumbnailpengumumanumum);
             holder.text_tanggal_tayang.setText(menu.getTgl_tayang_simple());
-            Glide.with(MainActivity.this).load(menu.getThumbnail()).into(holder.tumbnailpengumumanumum);
             holder.text_judul_pengumuman.setText(menu.getJudul_pengumuman());
             holder.text_isi_pengumuman.setText(menu.getIsi_pengumuman());
             holder.text_tgl_tayang_pengumuman.setText(menu.getTgl_tayang_pengumuman());
@@ -351,7 +223,7 @@ public class MainActivity extends AppCompatActivity {
             holder.cv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent i = new Intent(MainActivity.this, PengumumanActivity.class);
+                    Intent i = new Intent(Search.this, PengumumanActivity.class);
                     i.putExtra("id_pengumuman", menu.getId_pengumuman());
                     startActivity(i);
                 }
@@ -381,7 +253,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
 
     private void showDialog() {
         if (!pDialog.isShowing())
